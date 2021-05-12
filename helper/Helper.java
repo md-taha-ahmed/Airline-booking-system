d8832bdba90268290bc5bb7ac8eb9e83fc2762c4
md taha ahmed <md.taha.ahmed.projects@gmail.com>
2021-05-12 14:49:46 +0530

    fixed negative ids

--- helper/Helper.java
+++ helper/Helper.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class Helper {
     public static int getId() {
         Random random = new Random();
-        return random.nextInt();
+        return random.nextInt(Integer.SIZE - 1);
     }
 
     public static final Scanner scan = new Scanner(System.in);
