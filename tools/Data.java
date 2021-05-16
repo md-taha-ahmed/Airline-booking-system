d3e7b0112e2bd66c1be32476504f41f05f225a53
md taha ahmed <md.taha.ahmed.projects@gmail.com>
2021-05-16 21:35:58 +0530

    create a class for Gson method

---
+++ tools/Data.java
@@ -0,0 +1,50 @@
+package tools;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import com.google.gson.Gson;
+
+import controllers.BookingController;
+
+public class Data {
+    public static BookingController loadData() {
+        BookingController controller = new BookingController();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("data.json"));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+            String ls = System.getProperty("line.separator");
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+            // delete the last new line separator
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            reader.close();
+
+            String content = stringBuilder.toString();
+
+            Gson gson = new Gson();
+            controller = gson.fromJson(content, BookingController.class);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return controller;
+    }
+
+    public static boolean savingData(String data) {
+        try {
+            FileWriter fw = new FileWriter("data.json");
+            fw.write(data);
+            fw.close();
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+}
