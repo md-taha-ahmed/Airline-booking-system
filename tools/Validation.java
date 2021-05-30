a08696465dac64568b8024663501cbb2a37ba37a
md taha ahmed <md.taha.ahmed.projects@gmail.com>
2021-05-30 14:50:00 +0530

    fix validateDateTime()( return the correct String)

--- tools/Validation.java
+++ tools/Validation.java
@@ -4,28 +4,31 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class Validation {
-    public static void validateDateTime(String input) {
+    public static String validateDateTime(String input) {
+        String temp;
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         try {
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
             format.parse(input);
+            return input;
         } catch (ParseException e) {
             String newInput;
             System.out.println(
                     "You've Entered an  invalid date and time format please Enter the date and time in this format yyyy-MM-dd HH:mm ");
-                    System.out.print("");
-                        newInput = Helper.scan.nextLine();
-            validateDateTime(newInput);
+            System.out.print("");
+            newInput = Helper.scan.nextLine();
+            temp = validateDateTime(newInput);
         }
+        return temp;
     }
-    
-    public static boolean intIsEmpty(int input){
-        int number=-1;
-        number=input;
-        if(number==-1){
+
+    public static boolean intIsEmpty(int input) {
+        int number = -1;
+        number = input;
+        if (number == -1) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    
+
 }
